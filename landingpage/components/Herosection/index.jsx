770604b8c7fe14f6d5998db1c4a99933fee59d7b
adarshdev <Adarshkatoch82@gmail.com>
2025-06-30 11:01:20 +0530

    Navbar and Footer Added

---
+++ landingpage/components/Herosection/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const HeroSection = () => {
+  return (
+    <div>
+      <h1 className='text-center font-bold text-red-400'>Herosection</h1>
+    </div>
+  )
+}
+
+export default HeroSection
